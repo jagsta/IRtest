32f4e7a0e0c5114c1cbc5b5e30b5faeedf9992f8
Jag Bains <git@jagbains.org>
2014-05-07 23:03:00 +0100

    NEC protocol up down and repeat

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,6 +1,7 @@
 #include <IRremote.h>
 IRrecv irrecv(11); // Receive on pin 11
 decode_results results;
+int last;
 
 void setup()
 {
@@ -11,6 +12,32 @@ void setup()
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
+    if (results.value == 0xFFA857) { 
+      Serial.println("DOWN"); 
+      last = 1;
+    }
+    else if (results.value == 0xFF906F) { 
+      Serial.println("UP"); 
+      last = 2;
+    }
+    else if (results.value == 0xFFFFFFFF) {
+      Serial.print("REPEAT ");
+      switch ( last ) {
+	case 0:
+          Serial.println("IGNORE");
+	  break;
+	case 1:
+          Serial.println("DOWN");
+	  break;
+	case 2:
+          Serial.println("UP");
+	  break;
+      }
+    }
+    else {
+      Serial.println("IGNORE");
+      last = 0;
+    }
     irrecv.resume(); // Continue receiving
   }
 }
